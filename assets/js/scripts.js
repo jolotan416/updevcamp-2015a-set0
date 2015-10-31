b492da4df75be97e653f734b27b4f1c9d464579f
Jolo Tan <jolo_ck@yahoo.com.ph>
2015-11-01 01:17:53 +0800

    Scroll Animation Added (Website still Unresponsive)

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,2 +1,8 @@
 $(function(){
+  $('a').click(function(){
+      $('html, body').animate({
+          scrollTop: $( $.attr(this, 'href') ).offset().top
+      }, 500);
+      return false;
+  });
 });
